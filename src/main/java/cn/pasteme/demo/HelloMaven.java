4e4bb4bb3adb64884721d600abdf3473566da161
Lucien Shui <lucien@lucien.ink>
2019-10-03 17:40:02 +0800

    Project init

---
+++ src/main/java/cn/pasteme/demo/HelloMaven.java
@@ -0,0 +1,11 @@
+package cn.pasteme.demo;
+
+/**
+ * @author Lucien
+ * @date 2019/10/03 16:19
+ */
+public class HelloMaven {
+    public static void run() {
+        System.out.println("Hello Maven!");
+    }
+}
